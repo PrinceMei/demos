e4829a4b614d67d6dd8ce12e66edf90e1d865f99
jantoChan <czengtao@163.com>
2018-02-06 17:52:06 +0800

    图片懒加载增加两种方式

--- app/router/index.js
+++ app/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/components/home/index'
 import lazyLoad from '@/components/lazy-load/index'
+import adapter from '@/components/adapter/index'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/',
       name: 'lazyLoad',
       component: lazyLoad
+    },
+    {
+      path: '/adapter',
+      name: 'adapter',
+      component: adapter
     }
 
   ]
